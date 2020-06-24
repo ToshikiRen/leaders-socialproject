7b58e5988fc342958b63c89c15f5a7d8c9c3a920
Necula Leonard <neculaleonardgabriel@gmail.com>
2020-06-24 10:01:40 +0300

    Initial deploy

--- app.py
+++ app.py
@@ -32,6 +32,23 @@ class Feedback(db.Model):
         self.rating = rating
         self.comments = comments
 
+
+class New(db.Model):
+    
+    __tablename__ = 'new'
+    id = db.Column(db.Integer, primary_key = True)
+    customer = db.Column(db.String(200), unique = True)
+    dealer = db.Column(db.String(200), )
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+        
+
 @app.route('/')
 def index():
     return render_template('index.html')
