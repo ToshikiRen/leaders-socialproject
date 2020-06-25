483a8ba2bbd286616bf44516991b1adb64a19970
Necula Leonard <neculaleonardgabriel@gmail.com>
2020-06-25 23:58:11 +0300

    Initial deploy

--- app.py
+++ app.py
@@ -85,12 +85,13 @@ def login():
         remember = request.form['remember']
         if (db.session.query(New).filter(New.username == username).count() and
             db.session.query(New).filter(New.password == password).count()):
-            resp = make_response(redirect('/login_succes'))
-            resp.set_cookie('username', username, max_age=COOKIE_TIME_OUT)
-            resp.set_cookie('password', password, max_age=COOKIE_TIME_OUT)
-            resp.set_cookie('remember', 'checked', max_age=COOKIE_TIME_OUT)
-            return resp
-            # return render_template('index.html')
+            if remember:
+                resp = make_response(redirect('/login_succes'))
+                resp.set_cookie('username', username, max_age=COOKIE_TIME_OUT)
+                resp.set_cookie('password', password, max_age=COOKIE_TIME_OUT)
+                resp.set_cookie('remember', 'checked', max_age=COOKIE_TIME_OUT)
+                return resp
+            return render_template('index.html')
         return render_template('login.html', message = 'Date invalide')
 
 # Asta ruleaza cand apasam pe Sign Up
